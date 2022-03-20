8c73a1416c7549f621fe41d7fc1c7ff1c2777363
avillobo2010 <avillobo@gmail.com>
2022-03-20 13:59:28 +0530

    code refactoring

--- scripts/fix_previleges_pcs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-chmod +x /home/ec2-user/deployments/profile-consumer-service-0.0.1-SNAPSHOT.jar
-chmod +x /home/ec2-user/deployments/start_server_pcs.sh
-chmod +x /home/ec2-user/deployments/stop_server_pcs.sh
-
